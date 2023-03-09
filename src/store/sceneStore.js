be3cba77c1be99ee0421c285e54d81966f7591f0
dev-liaojianxin <it-liaojx@derucci.net>
2023-03-09 21:20:27 +0800

    feat：新增sceneStore

---
+++ src/store/sceneStore.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia'
+import { reactive, ref } from 'vue'
+
+import { getStorage, removeStorage, setStorage } from '@/utils/storage.js'
+
+export default defineStore('sceneStore', () => {
+  // 统一token处理
+
+  return {}
+})
