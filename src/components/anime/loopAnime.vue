efc61975debadffb7129901e447678f11949f8c7
dev-liaojianxin <it-liaojx@derucci.net>
2023-03-09 16:29:59 +0800

    feat：添加animejs和组件

---
+++ src/components/anime/loopAnime.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import anime from 'animejs/lib/anime.es.js'
+// import gsap from 'gsap'
+
+const tl = ref(null)
+
+const init = () => {
+  const wrapperEl = document.querySelector('.wrapper')
+  const numberOfEls = 90
+  const duration = 6000
+  const delay = duration / numberOfEls
+
+  tl.value = anime.timeline({
+    duration: delay,
+    complete: function () {
+      tl.value.restart()
+    }
+  })
+
+  function createEl(i) {
+    let el = document.createElement('div')
+    const rotate = (360 / numberOfEls) * i
+    const translateY = -50
+    const hue = Math.round((360 / numberOfEls) * i)
+    el.classList.add('el')
+    el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)'
+    el.style.transform =
+      'rotate(' + rotate + 'deg) translateY(' + translateY + '%)'
+    tl.value.add({
+      begin: function () {
+        anime({
+          targets: el,
+          backgroundColor: [
+            'hsl(' + hue + ', 40%, 60%)',
+            'hsl(' + hue + ', 60%, 80%)'
+          ],
+          rotate: [rotate + 'deg', rotate + 10 + 'deg'],
+          translateY: [translateY + '%', translateY + 10 + '%'],
+          scale: [1, 1.25],
+          easing: 'easeInOutSine',
+          direction: 'alternate',
+          duration: duration * 0.1
+        })
+      }
+    })
+    wrapperEl.appendChild(el)
+  }
+
+  for (let i = 0; i < numberOfEls; i++) createEl(i)
+}
+
+onMounted(() => {
+  init()
+})
+
+const restart = () => tl.value.restart()
+
+const pause = () => tl.value.pause()
+
+defineExpose({ restart, pause })
+</script>
+
+<template>
+  <div class="view">
+    <div class="wrapper"></div>
+  </div>
+</template>
+
+<style>
+.view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 1px;
+  height: 100%;
+}
+
+.el {
+  position: absolute;
+  opacity: 1;
+  width: 2px;
+  height: 12vh;
+  margin-top: -12vh;
+  transform-origin: 50% 100%;
+  background: white;
+}
+</style>
--- src/pages/house/houseAddDevice.vue
+++ src/pages/house/houseAddDevice.vue
@@ -1,17 +1,22 @@
 <script setup>
 import { ref } from 'vue'
+
+import LoopAnime from '@/components/loopAnime.vue'
+
+const animeRef = ref(null)
 </script>
 
 <template>
   <div class="min-h-screen bg-page-gray">
     <HeaderNavbar title="添加设备" />
     <div class="flex items-center justify-center">
-      <van-image
+      <!-- <van-image
         width="20rem"
         height="20rem"
         fit="contain"
         src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-      />
+      /> -->
+      <LoopAnime ref="animeRef" />
     </div>
     <div class="text-center">正在扫描附件的设备...</div>
   </div>
