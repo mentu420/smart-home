8fb56912e5b6ee3c402fe079c2836c0c1c7d3c1d
dev-liaojianxin <it-liaojx@derucci.net>
2023-03-13 20:14:51 +0800

    feat：新增api文件

--- src/apis/common/tabbar.js
+++
@@ -1,6 +0,0 @@
-import { request } from '@/utils/request/'
-
-export const login = (data) => request({ url: 'YongHu.aspx', data, method: 'POST' })
-//短信
-//type:1 身份验证；2短信测试；3登陆确认验证码；4登陆异常验证码；5用户注册验证码;6活动确认验证码；7修改密码验证码；8信息变更验证码；9分享钥匙
-export const getCode = (params) => request({ url: 'DuanXin.aspx', params })
---
+++ src/apis/house.js
@@ -0,0 +1,29 @@
+import { request } from '@/utils/request/'
+
+/**
+ * 房屋成员相关接口。op=1添加、op=2编辑、op=3删除、op=4获取
+ * **/
+export const setHouseMember = (data) => request({ url: '/ChengYuan.aspx', data, method: 'POST' })
+
+export const getHouseMember = (params) => request({ url: '/ChengYuan.aspx', params })
+
+/**
+ * 房屋相关接口。op=1获取、op=2新增、op=3编辑、op=4删除、op=5 切换
+ * **/
+export const getHouseList = (params) => request({ url: '/FangWu.aspx', params })
+
+export const setHouseList = (data) => request({ url: '/FangWu.aspx', data, method: 'POST' })
+
+/**
+ * 房间相关接口。op=1获取、op=2新增、op=3编辑、op=4删除
+ * **/
+export const getRoomList = (params) => request({ url: '/FangJian.aspx', params })
+
+export const setRoomList = (data) => request({ url: '/FangJian.aspx', data, method: 'POST' })
+
+/**
+ * 房间相关接口。op=1获取、op=2新增、op=3编辑、op=4删除
+ * **/
+export const getAreaList = (params) => request({ url: '/QuYu.aspx', params })
+
+export const setAreaList = (data) => request({ url: '/QuYu.aspx', data, method: 'POST' })
