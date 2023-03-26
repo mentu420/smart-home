f84b48880842803443859b3b5a819c0429055438
dev-liaojianxin <it-liaojx@derucci.net>
2023-03-26 16:50:29 +0800

    feat：归档

--- src/pages/smart/smartDeviceInfo.vue
+++ src/pages/smart/smartDeviceInfo.vue
@@ -3,9 +3,34 @@ import { IconPark } from '@icon-park/vue-next/es/all'
 import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
+import PickerSearch from '@/components/common/PickerSearch.vue'
+
 const route = useRoute()
 const router = useRouter()
 const checked = ref(true)
+const showPicker = ref(false)
+
+const columns = [
+  // 第一列
+  [
+    { text: '周一', value: 'Monday' },
+    { text: '周二', value: 'Tuesday' },
+    { text: '周三', value: 'Wednesday' },
+    { text: '周四', value: 'Thursday' },
+    { text: '周五', value: 'Friday' },
+  ],
+  // 第二列
+  [
+    { text: '上午', value: 'Morning' },
+    { text: '下午', value: 'Afternoon' },
+    { text: '晚上', value: 'Evening' },
+  ],
+]
+
+const onPickerConfirm = ({ selectedValues }) => {
+  console.log('selectedValues', selectedValues)
+  showPicker.value = false
+}
 </script>
 
 <template>
@@ -14,11 +39,12 @@ const checked = ref(true)
     <section class="m-4 overflow-hidden rounded-xl">
       <van-cell-group>
         <van-cell title="设备名称" value="内容" is-link />
-        <van-cell title="所属空间" value="内容" is-link />
+        <van-cell title="所属空间" value="内容" is-link @click="showPicker = !showPicker" />
         <van-cell center title="常用设备">
           <van-switch v-model="checked" />
         </van-cell>
       </van-cell-group>
     </section>
+    <PickerSearch v-model:show="showPicker" :columns="columns" @confirm="onPickerConfirm" />
   </div>
 </template>
