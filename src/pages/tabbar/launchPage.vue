0ed0c04e10f1d8b4ab9e30f2e013fdcd7a837b03
it-liaojx <it-liaojx@derucci.net>
2023-08-15 11:54:57 +0800

    feat(启动页)：新增启动页

---
+++ src/pages/tabbar/launchPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineOptions({ name: 'LaunchPage' })
+
+const images = ref([
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
+])
+</script>
+
+<template>
+  <div class="w-screen h-screen overflow-hidden">
+    <van-swipe class="w-screen h-screen" :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="(imageItem, imageIndex) in images" :key="imageIndex">
+        <van-image class="w-screen h-screen" fit="cover" :src="imageItem" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
