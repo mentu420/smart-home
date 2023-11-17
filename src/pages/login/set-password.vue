7a800dd7f5b79e610ae4a65c8586cf5f8db72a1a
it-liaojx <it-liaojx@derucci.net>
2023-11-17 17:30:47 +0800

    fix(密码)：修复设置密码

--- src/pages/login/set-password.vue
+++ src/pages/login/set-password.vue
@@ -4,15 +4,30 @@ import { ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 
 import { setUserConfig } from '@/apis/commonApi'
-import { validPassword, trimFormat } from '@/hooks/useFormValidator.js'
+import { validPassword } from '@/hooks/useFormValidator.js'
 import { useLogout } from '@/hooks/useLogout'
 
 defineOptions({ name: 'SetPassword' })
 
 const router = useRouter()
 const route = useRoute()
-const password = ref(null)
+const form = ref({})
 const loading = ref(false)
+
+const vaildPasswordFormat = [
+  { required: true, message: '请填写密码' },
+  {
+    validator: (value) => validPassword(value),
+    message: '密码不能含有中文、全角字符、问号和空格。密码最短不能少于6位，密码最长不能超过32字符',
+  },
+  {
+    validator: () => {
+      const { password = '', repeatPassword = '' } = form.value
+      return !(password != '' && repeatPassword != '' && password != repeatPassword)
+    },
+    message: '密码与确认密码不一致',
+  },
+]
 
 const onSubmit = async () => {
   try {
@@ -21,7 +36,7 @@ const onSubmit = async () => {
     await setUserConfig({
       params: { op: 5 },
       data: {
-        mima: password.value,
+        mima: form.value.password,
         shoujihaoma: phone,
         yanzhengma: code,
       },
@@ -39,18 +54,18 @@ const onSubmit = async () => {
     <h1 class="my-8">设置新密码</h1>
     <van-form @submit="onSubmit">
       <van-field
-        v-model="password"
+        v-model="form.password"
         name="password"
         placeholder="密码"
         maxlength="18"
-        :rules="[
-          { required: true, message: '请填写密码' },
-          {
-            validator: (value) => validPassword(value),
-            message:
-              '密码不能含有中文、全角字符、问号和空格。密码最短不能少于6位，密码最长不能超过32字符',
-          },
-        ]"
+        :rules="vaildPasswordFormat"
+      />
+      <van-field
+        v-model="form.repeatPassword"
+        name="repeatPassword"
+        placeholder="确认密码"
+        maxlength="18"
+        :rules="vaildPasswordFormat"
       />
       <div class="my-10">
         <van-button
@@ -58,7 +73,7 @@ const onSubmit = async () => {
           block
           type="primary"
           native-type="submit"
-          :disabled="!password"
+          :disabled="!form.password || !form.repeatPassword"
           :loading="loading"
         >
           下一步
