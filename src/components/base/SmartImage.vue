f8de9b7417c032a879e8e1c5f3c8c72dd1d40fa9
it-liaojx <it-liaojx@derucci.net>
2024-01-23 10:25:57 +0800

    fix(图片)：优化图片读取本地缓存

--- src/components/base/SmartImage.vue
+++ src/components/base/SmartImage.vue
@@ -1,9 +1,5 @@
 <script setup>
-import { storeToRefs } from 'pinia'
-import { computed, ref, useAttrs, watch } from 'vue'
-
-import networkStore from '@/store/networkStore'
-import { acceptFileValidate, imageTypes } from '@/utils/common'
+import { computed, ref, useAttrs } from 'vue'
 
 defineOptions({ name: 'SmartImage' })
 
@@ -13,23 +9,18 @@ const _attrs = computed(() => ({
   ...attrs,
   src: localImage.value == '' ? attrs?.src : localImage.value,
 }))
-const { isOnline } = storeToRefs(networkStore())
 
+// 原生调用
 function getPhotolocalDone(ora, localUrl) {
   console.log('原生读取图片完成', localUrl)
   localImage.value = localUrl
 }
 
-watch(
-  () => isOnline.value,
-  (val) => {
-    if (val || !acceptFileValidate(attrs.src, imageTypes)) return
-    console.log('原生准备读取本地图片', attrs.src)
-    window?.jdwl?.getPhotolocal(attrs.src, 'img1')
-  }
-)
+const onLoad = () => {
+  window?.jdwl?.getPhotolocal(attrs.src, 'img1')
+}
 </script>
 
 <template>
-  <van-image v-bind="_attrs" />
+  <van-image v-bind="_attrs" @load="onLoad" />
 </template>
