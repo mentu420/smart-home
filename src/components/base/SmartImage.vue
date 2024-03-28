a3f4e18420055d6a4e301c1e4362393c27d56fef
dev-liaojianxin <dev-liaojianxin@example.com>
2024-03-28 16:31:38 +0800

    feat(store)：新增materialStore

--- src/components/base/SmartImage.vue
+++ src/components/base/SmartImage.vue
@@ -1,28 +1,47 @@
 <script setup>
-import { computed, ref, useAttrs, useSlots } from 'vue'
+import { storeToRefs } from 'pinia'
+import { computed, ref, useAttrs, useSlots, watch } from 'vue'
+import materialStore from '@/store/materialStore'
 
 defineOptions({ name: 'SmartImage' })
 
 const attrs = useAttrs()
 const localImage = ref(null)
+const { materialImages } = storeToRefs(materialStore())
 
 // 原生调用
 function getPhotolocalDone(ora, localUrl) {
   console.log('原生读取图片完成', ora, localUrl)
   localImage.value = localUrl
+  materialImages.value = { ...materialImages.value, ora: localUrl }
 }
-
+// 原生方法挂载
 window.getPhotolocalDone = getPhotolocalDone
 
-const onLoad = () => {
-  window?.jdwl?.getPhotolocal(attrs.src, 'img1')
+// 先获取缓存本地资源路径，没有就下载图片
+const onLoad = (key) => {
+  if (materialImages.value[key]) {
+    localImage.value = materialImages.value[key]
+    return
+  }
+  window?.jdwl?.getPhotolocal(key, key)
 }
+
+watch(
+  () => attrs,
+  (val) => {
+    if (!val.src) return
+    console.log('开始获取图片', val.src)
+    onLoad(val.src)
+  },
+  { immediate: true }
+)
 
 const src = computed(() => localImage.value || attrs?.src)
 </script>
 
 <template>
-  <van-image v-bind="attrs" :src="src" @load="onLoad">
+  <van-image v-bind="attrs" :src="src">
     <template #error>
       <slot name="error"></slot>
     </template>
---
+++ src/store/materialStore.js
@@ -0,0 +1,18 @@
+import localforage from 'localforage'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+const storeName = 'materialStore'
+
+export default defineStore(storeName, () => {
+  const materialImages = ref({}) //图片对象 key：网络url value:本地资源路径
+
+  const init = async () => {
+    const storeRes = JSON.parse(await localforage.getItem(storeName))
+    materialImages.value = storeRes?.materialImages ?? {}
+  }
+
+  init()
+
+  return { materialImages }
+})
