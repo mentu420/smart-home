62961db966cfe770c3f7e3aab794e9103f7d67f9
dev-liaojianxin <dev-liaojianxin@example.com>
2024-07-11 16:42:09 +0800

    fix: 关闭vconsole

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import useSize from '@/utils/flexible/useRem.js'
 import { setNativeMethods } from '@/utils/native/fn'
 import { NATIVE_FILES } from '@/enums/nativeEnums'
 
-if (import.meta.env.MODE === 'development') new VConsole()
+// if (import.meta.env.MODE === 'development') new VConsole()
 
 socketStore()
 useSize()
