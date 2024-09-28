b729e9d11b26eca0a6c52eb29a84e34c4435c42a
廖剑新 <dev-liaojianxin@example.com>
2024-09-28 09:51:34 +0800

    version updated

--- src/store/socketStore.js
+++ src/store/socketStore.js
@@ -276,7 +276,7 @@ export default defineStore('socketStore', () => {
    * 设备、网关在线状态改变订阅 online
    * ***/
   function onDeviceOnlineSubscribe(data) {
-    console.log('%c设备/网关在线接收主题', getLogStyle('blue'), data)
+    if (showLog.value) console.log('%c设备/网关在线接收主题', getLogStyle('blue'), data)
     const { bianhao, shifouwangguan, zaixianzhuangtai } = data
     const { deviceList, hostList } = storeToRefs(deviceStore())
     if (shifouwangguan) {
